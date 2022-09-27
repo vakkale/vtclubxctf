85b7949f7b913c4470b64e65a9532e2db515d38a
vakkale <volkanakkale@vt.edu>
2022-09-27 12:15:52 -0400

    curtain effect attached to the middle 3 buttons on the header menu

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,9 +1,11 @@
-import { React, Component } from 'react';
+import { React, Component, useState } from 'react';
 import './header.scss';
+import './curtains.scss';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo2.png';
 import { useScrollDirection } from 'react-use-scroll-direction'
-import Curtains from './selectors/curtains';
+import { wait } from '@testing-library/user-event/dist/utils';
+import { render } from '@testing-library/react';
 
 function reveal() {
 
@@ -16,27 +18,37 @@ function hide() {
 
 
 export default class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: "Header",
+            isActive: false
+        }
+        this.curtains = this.curtains.bind(this);
+    }
+
     curtains = (menuItem) => {
-        
         switch (menuItem) {
             case 'competition':
                 console.log('competition');
-
+                this.setState({ isActive: true });
                 break
             case 'training':
                 console.log('training');
-
+                this.setState({ isActive: true });
                 break
             case 'records':
                 console.log('records');
-
+                this.setState({ isActive: true });
                 break
         }
+        setTimeout(() => { this.setState({ isActive: false }); }, 2300);
     }
+
     render() {
+        const { isActive } = this.state;
         return (
             <>
-                <Curtains></Curtains>
                 <header id='header' style={{ display: 'flex', width: '100%' }}>
                     {/* DO NOT TOUCH. ALIGNMENT IS PERFECT */}
                     <Link style={{ alignSelf: 'center', textAlign: 'center' }} className='menuPic' to="/">
@@ -56,7 +68,25 @@ export default class Header extends Component {
                     <Link style={{ textAlign: 'left', left: '0', cursor: 'default' }} className='menuPic' to="/">
                     </Link>
                 </header>
+                {isActive && <Curtain></Curtain>}
             </>
         )
     };
+}
+
+const Curtain = () => {
+    return (
+        <>
+            <div className="curtainsContainer">
+                <div className="curtain small"></div>
+                <div className="curtain big"></div>
+                <div className="curtain big hide"></div>
+                <div className="curtain big hide"></div>
+                <div className="curtain big hide"></div>
+                <div className="curtain big hide"></div>
+                <div className="curtain big"></div>
+                <div className="curtain small"></div>
+            </div>
+        </>
+    );
 }
