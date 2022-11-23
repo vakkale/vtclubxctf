9c755ba42151b2498f10456721112c78867b93e7
vakkale <volkanakkale@vt.edu>
2022-11-22 22:59:42 -0500

    fixed?

--- src/home/components/slider/Slider.jsx
+++ src/home/components/slider/Slider.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Slider.scss'
+import './Slider.scss';
 import logo from '../../../assets/logo.png'
 
 export default class Slider extends Component {
