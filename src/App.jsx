f69d20ddea76912b04be7047377e61cae74e960b
vakkale <volkanakkale@vt.edu>
2022-12-20 21:15:49 -0500

    loading test 3?

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import React, { Fragment, Component } from 'react';
+import React, { Fragment } from 'react';
+import { useState, useEffect } from 'react';
 
 import './App.scss';
 import Lines from './lines/lines.jsx';
@@ -11,16 +12,48 @@ import favicon from './assets/favicon.png';
 import trainingData from './data/training.json';
 import trainingNav from './data/trainingNav.json';
 import News from './news/News';
-/* import Loader from './loader/loader.jsx';
-import { usePreloadImages } from './hooks/usePreloadImages.tsx'; */
+import loadImages from './helpers/loadImages';
 
-export default class App extends Component {
-  render() {
+import img1 from './wildcat22_adam_sprint.JPG';
+import img2 from './wildcat22_emery_vault3.JPG';
+import img3 from './chris_jump.JPG';
+import img4 from './sienna_track.JPG';
+import img5 from './mitch_baton.JPG';
+import img6 from './wildcat_team.jpg';
+
+export default function App() {
+
+  const imgs = [/* "./wildcat22_adam_sprint.JPG", "./wildcat22_emery_vault3.JPG", "./chris_jump.JPG", "./sienna_track.JPG", "./mitch_baton.JPG", "./wildcat_team.jpg" */img1, img2, img3, img4, img5, img6];
+
+  const [loading, setLoading] = useState(true);
+
+  /* AAAAAAAAAAAAAAAAAA */
+
+  useEffect(() => {
+    loadImages(imgs).then(() => {
+      setLoading(false);
+    });
+  });
+
+  const LoadingScreen = () => {
+    return (
+      <div className="article-loading-screen" style={{ backgroundColor: "white", width: "100%", height: "100%", zIndex: 999, position: "absolute" }}>
+        <div className="spinner"></div>
+      </div>
+    );
+  }
+
+  if (loading) {
+    return (
+      <div className="app">
+        <LoadingScreen></LoadingScreen>
+      </div>
+    );
+  }
+  else {
     return (
       <>
-        {/* <Loader></Loader> */}
         <div className="app">
-
           <Fragment>
             <Favicon url={favicon}></Favicon>
             <BrowserRouter>
--- src/training/training.jsx
+++ src/training/training.jsx
@@ -2,10 +2,7 @@ import { React, Component } from 'react';
 import './training.scss';
 import '../helpers/side-menu/SideMenu.scss'
 import trainingData from '../data/training.json';
-import Loader from '../loader/loader';
 import SideMenu from '../helpers/side-menu/SideMenu.jsx';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
 
 export default class Training extends Component {
     constructor(props) {
@@ -24,60 +21,6 @@ export default class Training extends Component {
     }
 
     render() {
-
-        /* const SideMenu = ({ menuItems }) => {
-            const depthLevel = 0;
-            return (
-                <ul className="menus">
-                    {menuItems.map((menu, index) => {
-                        const depthLevel = 0;
-                        return <MenuItems items={menu} key={index} />;
-                    })}
-                </ul>
-            );
-        }
-
-        const MenuItems = ({ items, depthLevel }) => {
-            const [dropdown, setDropdown] = useState(false);
-            return (
-                <li className="menu-items">
-                    {items.children ? (
-                        <>
-                            <button
-                                aria-expanded={dropdown ? "true" : "false"}
-                                onClick={() => setDropdown((prev) => !prev)}
-                                type="button" aria-haspopup="menu"
-                            >
-                                {depthLevel > 0 ? <span>&raquo;</span> : <span className="arrow" />}
-                                {items.name}{' '}
-                            </button>
-                            <Dropdown
-                                submenus={items.children}
-                                dropdown={dropdown}
-                            />
-                        </>
-                    ) : (
-                        <Link to={`${items.id}`} onClick={() => this.changeState(items)}>{items.name}</Link>
-                    )
-                    }
-                </li >
-            );
-        }
-
-        const Dropdown = ({ submenus, dropdown, depthLevel }) => {
-            depthLevel = depthLevel + 1;
-            const dropdownClass = depthLevel > 1 ? "dropdown-submenu" : "";
-            return (
-                <ul className={`dropdown ${dropdownClass} ${dropdown ? "show" : ""}`}>
-                    {submenus.map((submenu, index) => (
-                        <MenuItems
-                            items={submenu}
-                            key={index}
-                            depthLevel={depthLevel} />
-                    ))}
-                </ul>
-            );
-        } */
 
         const Header = () => {
             return (
@@ -99,12 +42,12 @@ export default class Training extends Component {
         const Plan = () => {
             return (
                 <div className="planWrapper">
-                    <iframe 
-                    src={this.state.currentPlan.url} 
-                    title="plan"
-                    width="100%" 
-                    height="100%" 
-                    frameBorder="0"
+                    <iframe
+                        src={this.state.currentPlan.url}
+                        title="plan"
+                        width="100%"
+                        height="100%"
+                        frameBorder="0"
                     />
                 </div>
             );
@@ -112,7 +55,6 @@ export default class Training extends Component {
 
         return (
             <>
-                {this.state.mount && <Loader></Loader>}
                 < div className="wrapper" key={this.props.key} >
                     <Header className="header" />
                     <Menu className="menu" />
