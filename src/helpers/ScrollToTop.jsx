606a54a6cc23d020dc9b77bb5dc31cf4dc76f221
vakkale <volkanakkale@vt.edu>
2023-01-17 02:46:35 -0500

    refined scrolltotop behavior. doesnt scroll to top if within the same page

--- src/helpers/ScrollToTop.jsx
+++ src/helpers/ScrollToTop.jsx
@@ -1,11 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
     const { pathname } = useLocation();
 
+    //set domain equal to between the first and last slash
+    const domain = pathname.substring(pathname.indexOf("/") + 1, pathname.lastIndexOf("/"));
+    console.log(domain);
+
+    const domainRef = useRef(null);
+
     useEffect(() => {
-        window.scrollTo(0, 0);
+        //if domain changes
+        if ((domainRef.current !== domain) || (domain === "competition") || (domain === "community")) {
+            window.scrollTo(0, 0);
+        }
+        domainRef.current = domain;
     }, [pathname]);
 
     return null;
