89bd23c6afda8d8841389bbce9d813b71d80804f
vakkale <volkanakkale@vt.edu>
2023-07-10 17:57:16 -0400

    updated officers minus charlotte's bio

--- src/data/officers.jsx
+++ src/data/officers.jsx
@@ -1,5 +1,6 @@
 const Officers = [
     /* {
+        RIP Phil
         position: "President",
         name: "Phil Lopez",
         email: "lphilip01@vt.edu",
@@ -10,7 +11,7 @@ const Officers = [
         position: "President",
         name: "Wyatt Thomas",
         email: "wyatt@vt.edu",
-        image: "https://i.imgur.com/vxSWd0H.jpg",
+        image: "https://i.imgur.com/5B4zeiMl.jpg",
         bio: "Hey there! My name is Wyatt and I’m the President of the club! A little bit about myself is that I’m a sophomore majoring in both physics and math and I’m a distance runner, so I love a good 5k and I have all the quirks that distance guys have. Being President is a really fun and rewarding position, and I love getting to know everyone. Our club is such a fun, tight knit community that makes showing up every day worth it, from the painful workouts to our bumpin’ socials. Please feel free to email me if you have any questions and I can’t wait to see you out on the track!"
     },
     {
@@ -23,73 +24,80 @@ const Officers = [
     },
     {
         position: "Race Coordinator",
-        name: "Hannah Cannon",
-        email: "hcannon@vt.edu",
-        image: "https://i.imgur.com/m8dDc1M.jpg",
-        bio: "Hi everyone, my name is Hannah and I’m the race coordinator! I have the honor of getting to plan our meet schedule and travel arrangements. I love meets because they are a great way to connect with others on the club while enjoying our sport at any competition level that you’d like! A little about me: I’m a graduating senior with a major in biochemistry and minor in mathematics. I went to high school in Prince William County in Northern Virginia, but moved around my whole life because my dad was in the Air Force. Besides running, I love going to coffee shops, hiking, and spending time with friends/family!"
+        name: "Roman Ciulla",
+        email: "romanc03@vt.edu",
+        image: "https://i.imgur.com/roA0rxCl.jpg",
+        bio: "Hello! My name is Roman, your race coordinator for the upcoming cross country and track seasons! My role consists of being in charge of planning our meets and arranging transportation to them. I’m majoring in Civil Engineering with a focus in transportation. Running has been a big part of my life since my sophomore year of high school, through this awesome club I’ve been able to stick with it through college! I can’t wait to see what we all do at our meets this year, but remember, at the end of day it will all be for fun!!"
+    },
+    {
+        position: "Treasurer",
+        name: "Evelyn Tipper",
+        email: "etipper@vt.edu",
+        image: "https://i.imgur.com/EDwsAFJl.jpg",
+        bio: "Hi everyone! My name is Evelyn and I’m excited to be the club treasurer this upcoming school year. I’m from Pittsburgh, PA and run distance with my favorite events being the 1500m and steeplechase. I’m a junior majoring in Biological Systems Engineering with a minor in Environmental Science. A fun fact about me is that I’m super passionate about watersheds and all things water; I will tell you if we enter a new watershed while driving to meets. Looking forward to an awesome year of taking easy runs easy, competing, and spending time with the team!"
+    },
+    {
+        position: "Women's Distance Captain",
+        name: "Sydney Szabos",
+        email: "sydneysz@vt.edu",
+        image: "https://i.imgur.com/cl5eMNSl.jpg",
+        bio: "WHAT’S UP GUYS, Sydney here, and I’m so excited to be your women’s distance captain for the 2023-24 season! I’m a senior majoring in computer engineering and minoring in computer science and math (but that doesn’t mean I can count my reps, unfortunately). I love a good cross country race, of course, and in track I compete mostly in the 1500 and 5k with an occasional struggle through the 800 or even 4x400. Club is where I’ve made some of my first and best friends and I’m so, so excited for this upcoming year and so honored that I get to serve in this role. I can’t wait to see everyone in the fall!!!"
+    },
+    {
+        position: "Men's Distance Captain",
+        name: "Ben Darden",
+        email: "bdarden1205@vt.edu",
+        image: "https://i.imgur.com/MaDBKMzl.jpg",
+        bio: "Hey everyone, Ben here! I'm back for round 2 as the Men's Distance Captain. I'm a first year PhD student in Engineering Mechanics. Club has been such a big part of my life the last several years. It's such a great community of people! Feel free to message me if you have any questions regarding running, club, or just life in general. I'm looking forward to starting another great semester!"
+    },
+    {
+        position: "Sprints Co-Captain",
+        name: "Adam Defosse",
+        email: "apdefosse@vt.edu",
+        image: "https://i.imgur.com/jDOlOagl.jpg",
+        bio: "Hello everyone, my name is Adam DeFosse and I’m one of the sprint captains this year. I’m currently a senior majoring in Marketing. Some of my interests include gaming, reading, and spending time with friends. My favorite thing about the club has been all the new people I’ve gotten to meet over the past couple of years, and I look forward to building more friendships during the remainder of my time here at Tech."
+    },
+    {
+        position: "Sprints Co-Captain",
+        name: "Riley Ophof",
+        email: "oriley@vt.edu",
+        image: "https://i.imgur.com/JuSUG5Kl.jpg",
+        bio: "Hello everyone, my name is Riley Ophof and I’m one of the sprint captains this year. I’m a rising sophomore majoring in Chemistry. Some of my interests include gaming, crosswords, and spending time with friends. My favorite thing about the club has been all the new people I’ve gotten to meet over the past year, and I look forward to building more friendships during the rest of my time here at Tech."
+    },
+    {
+        position: "Throws Captain",
+        name: "Nyle Sheriff",
+        email: "nylesheriff@vt.edu",
+        image: "https://i.imgur.com/vTg3Pa7l.jpg",
+        bio: "Hey everyone, my name is Nyle and I'm about to be a third year architecture student. I'm super excited to be coming back as your throws captain and can't wait to meet everyone in the fall! Our thrower family loves having a great time wherever we may be so come find us and give us a shot - we'd love to have you join us for this upcoming year!"
+    },
+    {
+        position: "Jumps Captain",
+        name: "Aidan McCrory",
+        email: "amccrory@vt.edu",
+        image: "https://i.imgur.com/oOBYswjl.jpg",
+        bio: "Hi everyone! My name is Aidan, and I’ll be the jumps captain this year. I am a junior majoring in chemical engineering. This is will my second year being in this position and I’m very excited to say that. I compete in the long and triple jump as well as the sprint events. Looking forward to meeting everyone and having a successful year!"
+    },
+    {
+        position: "Webmaster",
+        name: "Charlotte Baxter",
+        email: "cbaxter@vt.edu",
+        image: "https://i.imgur.com/tNqZEuBl.jpg",
+        bio: "Hi everyone! My name is Charlotte and I don't have a bio ready yet 😋."
+    },
+    {
+        position: "Apparel Officer",
+        name: "Molly Donaldson",
+        email: "mollyd@vt.edu",
+        image: "https://i.imgur.com/qgcgIHSl.jpg",
+        bio: "Hello my favorite people!! I am Molly your apparel officer! woooo!! I am a rising junior from Delaware. I am sooo excited for this year! LETS GOOOOOOO! I'm a distance girly (6k/5k,1500,800 slay) who isn't the biggest running fan, but I love you all so now I love it tehe. I cannot wait to see you all this year and take home some Ws and epic memories. I also hope to meet some lovely new faces. <3 "
     },
     {
         position: "Social Coordinator",
         name: "Harris Naseh",
         email: "harrisn24@vt.edu",
         image: "https://i.imgur.com/T6Zkpwx.jpg",
-        bio: "Hey, I’m Harris Naseh, your social coordinator. I’m from Charlottesville, VA and majoring in computer science. I race the 1500-8k but I’m gonna do the 800 to beat Justin Moses. I am excited to get racing and do the social events I have planned like our Jumps/Sprints/Field vs Distance soccer game!"
+        bio: "Hey Everyone! My name is Harris and I'm your Social Coordinator this year. I'm a senior from Charlottesville, VA majoring in Computer Science. This is my second year as Social Coordinator and I look forward to having bigger and better events. I love being involved with the club and getting to know the new faces. I race anything between the 1500 and 10k. I look forward to seeing everyone in Blacksburg! "
     },
-    {
-        position: "Women's Distance Captain",
-        name: "Alyssa McCaffrey",
-        email: "alyssam20@vt.edu",
-        image: "https://i.imgur.com/AkcPTEP.jpg",
-        bio: "Hi!! I’m Alyssa, the woman’s distance captain this year! I ran XC and track throughout high school and have training experience from great coaches. I typically run the 800m and 1600m but have learned to enjoy racing the 6k this past XC season. I can’t wait to train this spring with you all and help lead our women to a national title! See you all at practice and feel free to reach out to me with any questions!"
-    },
-    {
-        position: "Men's Distance Captain",
-        name: "Ben Darden",
-        email: "bdarden1205@vt.edu",
-        image: "https://i.imgur.com/ocdHbQG.jpg",
-        bio: "Hey everyone! I'm Ben and I'm the Men's Distance Captain. I'm a 3rd year senior in Biomedical Engineering from Colorado Springs. My favorite thing about club (other than everything) is the amazing people in the club and the incredible community. Can't wait to establish the club dynasty this semester 😈 PS. If you want good jokes, ask Phil, Wyatt's jokes are terrible"
-    },
-    {
-        position: "Sprints Co-Captain",
-        name: "Emmy Donlon",
-        email: "emmyd@vt.edu",
-        image: "https://i.imgur.com/PLDQ2nf.jpg",
-        bio: "Hey y’all, my name is Emmy and I am one of your sprint captains this year!! I am a senior from Purcellville, Va (NoVa) majoring in Civil Engineering with a minor in Green Engineering here at Tech. My favorite event is the 200m, but I also run the 100m, 400m, and all the sprint relays! I’m so excited to train with everyone in Blacksburg this spring (especially to see the gorgeous sunsets during practice time)!! I’m looking forward to having an amazing track szn! Please feel free to reach out to me with questions, comments, or just to say hi!"
-    },
-    {
-        position: "Sprints Co-Captain",
-        name: "Adam Defosse",
-        email: "apdefosse@vt.edu",
-        image: "https://i.imgur.com/XtL3P0D.jpg1",
-        bio: "Hey everyone, my name is Adam DeFosse and I’m one of the sprint captains this year. I’m currently a junior majoring in Marketing. Some of my interests include gaming, reading, and spending time with friends. My favorite thing about the club has been all the new people I’ve gotten to meet over the past couple of years, and I look forward to building more friendships during the remainder of my time here at Tech."
-    },
-    {
-        position: "Throws Captain",
-        name: "Nyle Sheriff",
-        email: "nylesheriff@vt.edu",
-        image: "https://i.imgur.com/9Ggk8k8.jpg",
-        bio: "Hey everyone, my name is Nyle and I’m a second year architecture student. I’m the throws captain and I particularly love throwing discus - but compete in shotput and hammer throw too. When I’m not in the studio designing buildings you’ll probably find me at the track spinning in circles or at the gym moving heavy circles. I’m excited for this upcoming spring semester and look forward to seeing how far VT Club XCTF Throws can go!"
-    },
-    {
-        position: "Jumps Captain",
-        name: "Aidan McCrory",
-        email: "amccrory@vt.edu",
-        image: "https://i.imgur.com/050J6au.jpg",
-        bio: "Hi, I’m Aidan McCrory and I am the jumps captain for the Spring 2023 season. I am excited to get to know all of you and work to improve your jumping abilities. I am a sophomore in chemical engineering and a member of the Alpha Chi Sigma professional chemistry fraternity as well as the Chem-E-car design team. My favorite events are long and triple jump and my favorite part of this club is how friendly all the members are. Looking forward to meeting you all and winning a second national championship!"
-    },
-    {
-        position: "Apparel Officer",
-        name: "Suzie DiLorenzo",
-        email: "suzied9@vt.edu",
-        image: "https://i.imgur.com/9lX8vUZ.jpg?1",
-        bio: "Hi everyone, my name is Suzie and I’m the apparel officer! I design the uniforms and the apparel for the club. My favorite part of club are going to meets because they are a great get to know everyone and racing is always fun! A little about me: I’m a junior majoring in Human, Nutrition, Foods, and Exercise with a concentration in Dietetics, and I’m from Leesburg, VA. Besides running, I love to cook, do puzzles, drink coffee, and go on walks."
-    },
-    {
-        position: "Treasurer",
-        name: "Sam Derasse",
-        email: "samanthad@vt.edu",
-        image: "https://imgur.com/yqv0fdO.jpg",
-        bio: "Hello everyone, my name is Sam Derasse and I’m the club treasurer. I’m a senior in aerospace engineering from Long Island but I will be moving to Virginia once I graduate. This season I will be attempting the 3k steeple for the first time. My favorite part of club is being able to cheer on my teammates during all the different events! If you have any questions feel free to ask me."
-    }
 ]
 export default Officers;
